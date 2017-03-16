02bd33ea70910a5bc77c3bfa1d082aeb5e4d7e87
alvaro <gitkraken@alvaroprieto.com>
2017-03-15 19:55:15 -0700

    Added board file (first test with new board!)

---
+++ fw/board.h
@@ -0,0 +1,100 @@
+#ifndef __PINS_H__
+#define __PINS_H__
+
+#define _LED0_PORT GPIOC
+#define _LED0_PIN 13
+
+#define _LED1_PORT GPIOC
+#define _LED1_PIN 14
+
+#define _LED2_PORT GPIOC
+#define _LED2_PIN 15
+
+#define _DBG_LED_PORT GPIOF
+#define _DBG_LED_PIN 0
+
+#define _VBUS_DET_PORT GPIOA
+#define _VBUS_DET_PIN 0
+
+#define _VBUS_EXT_DET_PORT GPIOA
+#define _VBUS_EXT_DET_PIN 1
+
+#define _SNS1_PORT GPIOA
+#define _SNS1_PIN 2
+
+#define _SNS2_PORT GPIOA
+#define _SNS2_PIN 3
+
+#define _SNS3_PORT GPIOA
+#define _SNS3_PIN 4
+
+#define _OCS_N3_PORT GPIOA
+#define _OCS_N3_PIN 5
+
+#define _PRTPWR3_PORT GPIOA
+#define _PRTPWR3_PIN 6
+
+#define _OCS_N2_PORT GPIOA
+#define _OCS_N2_PIN 7
+
+#define _PRTPWR2_PORT GPIOB
+#define _PRTPWR2_PIN 0
+
+#define _OCS_N1_PORT GPIOB
+#define _OCS_N1_PIN 1
+
+#define _PRTPWR1_PORT GPIOB
+#define _PRTPWR1_PIN 2
+
+#define _USART3_TX_PORT GPIOB
+#define _USART3_TX_PIN 10
+
+#define _USART3_RX_PORT GPIOB
+#define _USART3_RX_PIN 11
+
+#define _5V_EN1_PORT GPIOB
+#define _5V_EN1_PIN 12
+
+#define _5V_EN2_PORT GPIOB
+#define _5V_EN2_PIN 13
+
+#define _5V_EN3_PORT GPIOB
+#define _5V_EN3_PIN 14
+
+#define _HUB_RST_N_PORT GPIOB
+#define _HUB_RST_N_PIN 15
+
+#define _5V_FLG1_PORT GPIOA
+#define _5V_FLG1_PIN 8
+
+#define _5V_FLG2_PORT GPIOA
+#define _5V_FLG2_PIN 9
+
+#define _5V_FLG3_PORT GPIOA
+#define _5V_FLG3_PIN 10
+
+#define _ST_USB_N_PORT GPIOA
+#define _ST_USB_N_PIN 11
+
+#define _ST_USB_P_PORT GPIOA
+#define _ST_USB_P_PIN 12
+
+#define _SWDIO_PORT GPIOA
+#define _SWDIO_PIN 13
+
+#define _SWCLK_PORT GPIOA
+#define _SWCLK_PIN 14
+
+#define _SUSP_IND_PORT GPIOB
+#define _SUSP_IND_PIN 3
+
+#define _CFG_SEL1_PORT GPIOB
+#define _CFG_SEL1_PIN 4
+
+#define _SCL_PORT GPIOB
+#define _SCL_PIN 6
+
+#define _SDA_PORT GPIOB
+#define _SDA_PIN 7
+
+#endif // __PINS_H__
--- fw/main.c
+++ fw/main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 
 #include "console.h"
+#include "board.h"
 #include "fifo.h"
 #include "stm32f0xx.h"
 #include "stm32f0xx_conf.h"
@@ -28,33 +29,32 @@ void init_uart() {
 	fifoInit(&txFifo, FIFO_BUFF_SIZE, outBuff);
 	fifoInit(&rxFifo, FIFO_BUFF_SIZE, inBuff);
 
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
 
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
-	GPIO_Init(GPIOA, &(GPIO_InitTypeDef){GPIO_Pin_9, GPIO_Mode_AF, GPIO_Speed_50MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
-	GPIO_Init(GPIOA, &(GPIO_InitTypeDef){GPIO_Pin_10, GPIO_Mode_AF, GPIO_Speed_50MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_1);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+
+	GPIO_Init(_USART3_TX_PORT, &(GPIO_InitTypeDef){(1 << _USART3_TX_PIN), GPIO_Mode_AF, GPIO_Speed_50MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
+	GPIO_Init(_USART3_RX_PORT, &(GPIO_InitTypeDef){(1 << _USART3_RX_PIN), GPIO_Mode_AF, GPIO_Speed_50MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
+	GPIO_PinAFConfig(_USART3_TX_PORT, _USART3_TX_PIN, GPIO_AF_4);
+	GPIO_PinAFConfig(_USART3_RX_PORT, _USART3_RX_PIN, GPIO_AF_4);
 
 	USART_StructInit(&uartConfig);
 
 	uartConfig.USART_BaudRate = 115200;
-	USART_Init(USART1, &uartConfig);
+	USART_Init(USART3, &uartConfig);
 
-	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-	USART_ITConfig(USART1, USART_IT_TXE, DISABLE);
+	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
+	USART_ITConfig(USART3, USART_IT_TXE, DISABLE);
 
-	USART_Cmd(USART1, ENABLE);
+	USART_Cmd(USART3, ENABLE);
 
-	NVIC_EnableIRQ(USART1_IRQn);
+	NVIC_EnableIRQ(USART3_4_IRQn);
 }
 
 int uartPutchar(USART_TypeDef *uart, fifo_t *fifo, char c) {
 	if(uart->ISR & USART_ISR_TXE) {
 		uart->TDR = c;
 		USART_ITConfig(uart, USART_IT_TXE, ENABLE);
-		GPIO_SetBits(GPIOB, GPIO_Pin_1);
-
 	} else {
 		fifoPush(fifo, c);
 	}
@@ -72,7 +72,7 @@ int _write (int fd, char *ptr, int len)
 	// If planning on supporting both serial and usb-serial, check fd here!
 	//
 	while(len--) {
-		uartPutchar(USART1, &txFifo, *ptr++);
+		uartPutchar(USART3, &txFifo, *ptr++);
 	}
 	return len;
 }
@@ -98,21 +98,25 @@ void init() {
 		while (1){};
 	}
 
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
-	GPIO_Init(GPIOB, &(GPIO_InitTypeDef){GPIO_Pin_0, GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
-	GPIO_Init(GPIOB, &(GPIO_InitTypeDef){GPIO_Pin_1, GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
-	GPIO_Init(GPIOB, &(GPIO_InitTypeDef){GPIO_Pin_2, GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOF, ENABLE);
+	GPIO_Init(_DBG_LED_PORT, &(GPIO_InitTypeDef){(1 << _DBG_LED_PIN), GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
+
+
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+	GPIO_Init(_LED0_PORT, &(GPIO_InitTypeDef){(1 << _LED0_PIN), GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
+	GPIO_Init(_LED1_PORT, &(GPIO_InitTypeDef){(1 << _LED1_PIN), GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
+	GPIO_Init(_LED2_PORT, &(GPIO_InitTypeDef){(1 << _LED2_PIN), GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
 
 	init_uart();
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
-	GPIO_Init(GPIOA, &(GPIO_InitTypeDef){GPIO_Pin_15, GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
-	GPIO_Init(GPIOB, &(GPIO_InitTypeDef){GPIO_Pin_0, GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
 
-	USBD_Init(&USB_Device_dev,
-			&USR_desc,
-			&USBD_CDC_cb,
-			&USR_cb);
+	GPIO_ResetBits(_LED0_PORT, (1 << _LED0_PIN));
+	GPIO_ResetBits(_LED1_PORT, (1 << _LED1_PIN));
+	GPIO_ResetBits(_LED2_PORT, (1 << _LED2_PIN));
+
+	// USBD_Init(&USB_Device_dev,
+	// 		&USR_desc,
+	// 		&USBD_CDC_cb,
+	// 		&USR_cb);
 }
 
 int main(void) {
@@ -121,17 +125,15 @@ int main(void) {
 
 	init();
 
-	printf("hello!\n");
-
 	nextBlink = tickMs + BLINK_DELAY_MS;
 	for(;;) {
 
 		if(tickMs > nextBlink) {
 			nextBlink = tickMs + BLINK_DELAY_MS;
 			if(blinkState) {
-				GPIO_SetBits(GPIOB, GPIO_Pin_0);
+				GPIO_SetBits(_DBG_LED_PORT, (1 << _DBG_LED_PIN));
 			} else {
-				GPIO_ResetBits(GPIOB, GPIO_Pin_0);
+				GPIO_ResetBits(_DBG_LED_PORT, (1 << _DBG_LED_PIN));
 			}
 			blinkState ^= 1;
 		}
@@ -149,26 +151,24 @@ void SysTick_Handler(void)
 	tickMs++;
 }
 
-void USART1_IRQHandler(void) {
-	uint32_t irq = USART1->ISR;
+void USART3_4_IRQHandler(void) {
+	uint32_t irq = USART3->ISR;
+
 	if(irq & USART_ISR_TXE) {
 		// Tx new byte if available
 		if(fifoSize(&txFifo) > 0) {
-			USART1->TDR = fifoPop(&txFifo);
+			USART3->TDR = fifoPop(&txFifo);
 
 		} else {
-			USART_ITConfig(USART1, USART_IT_TXE, DISABLE);
-			GPIO_ResetBits(GPIOB, GPIO_Pin_1);
-
+			USART_ITConfig(USART3, USART_IT_TXE, DISABLE);
 		}
 	}
 
 	if(irq & USART_ISR_RXNE) {
 		// Add received char to buff
-		fifoPush(&rxFifo, USART1->RDR);
-		GPIO_SetBits(GPIOB, GPIO_Pin_2);
+		fifoPush(&rxFifo, USART3->RDR);
 	}
 
-	USART1->ICR = irq;
+	USART3->ICR = irq;
 }
 
