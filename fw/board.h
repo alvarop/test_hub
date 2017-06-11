9c1f36250b7ee01614d738ff3c6daab0202eb0d0
alvaro <gitkraken@alvaroprieto.com>
2017-06-10 20:49:16 -0700

    Now working with test hub v0.2 with uart

--- fw/board.h
+++ fw/board.h
@@ -1,32 +1,32 @@
 #ifndef __PINS_H__
 #define __PINS_H__
 
-#define _LED0_PORT GPIOC
-#define _LED0_PIN 13
-
 #define _LED1_PORT GPIOC
-#define _LED1_PIN 14
+#define _LED1_PIN 13
 
 #define _LED2_PORT GPIOC
-#define _LED2_PIN 15
+#define _LED2_PIN 14
+
+#define _LED3_PORT GPIOC
+#define _LED3_PIN 15
 
 #define _DBG_LED_PORT GPIOF
 #define _DBG_LED_PIN 0
 
-#define _VBUS_DET_PORT GPIOA
-#define _VBUS_DET_PIN 0
-
 #define _VBUS_EXT_DET_PORT GPIOA
-#define _VBUS_EXT_DET_PIN 1
+#define _VBUS_EXT_DET_PIN 0
 
-#define _SNS1_PORT GPIOA
-#define _SNS1_PIN 2
+#define _SUSP_IND_PORT GPIOA
+#define _SUSP_IND_PIN 1
 
-#define _SNS2_PORT GPIOA
-#define _SNS2_PIN 3
+#define _VBUS_DET_PORT GPIOA
+#define _VBUS_DET_PIN 2
 
-#define _SNS3_PORT GPIOA
-#define _SNS3_PIN 4
+#define _HUB_RST_N_PORT GPIOA
+#define _HUB_RST_N_PIN 3
+
+#define _CFG_SEL1_PORT GPIOA
+#define _CFG_SEL1_PIN 4
 
 #define _OCS_N3_PORT GPIOA
 #define _OCS_N3_PIN 5
@@ -55,23 +55,20 @@
 #define _5V_EN1_PORT GPIOB
 #define _5V_EN1_PIN 12
 
+#define _5V_FLG1_PORT GPIOB
+#define _5V_FLG1_PIN 13
+
 #define _5V_EN2_PORT GPIOB
-#define _5V_EN2_PIN 13
+#define _5V_EN2_PIN 14
 
-#define _5V_EN3_PORT GPIOB
-#define _5V_EN3_PIN 14
+#define _5V_FLG2_PORT GPIOB
+#define _5V_FLG2_PIN 15
 
-#define _HUB_RST_N_PORT GPIOB
-#define _HUB_RST_N_PIN 15
-
-#define _5V_FLG1_PORT GPIOA
-#define _5V_FLG1_PIN 8
-
-#define _5V_FLG2_PORT GPIOA
-#define _5V_FLG2_PIN 9
+#define _5V_EN3_PORT GPIOA
+#define _5V_EN3_PIN 8
 
 #define _5V_FLG3_PORT GPIOA
-#define _5V_FLG3_PIN 10
+#define _5V_FLG3_PIN 9
 
 #define _ST_USB_N_PORT GPIOA
 #define _ST_USB_N_PIN 11
@@ -84,12 +81,6 @@
 
 #define _SWCLK_PORT GPIOA
 #define _SWCLK_PIN 14
-
-#define _SUSP_IND_PORT GPIOB
-#define _SUSP_IND_PIN 3
-
-#define _CFG_SEL1_PORT GPIOB
-#define _CFG_SEL1_PIN 4
 
 #define _SCL_PORT GPIOB
 #define _SCL_PIN 6
--- fw/main.c
+++ fw/main.c
@@ -18,42 +18,42 @@ volatile uint32_t tickMs = 0;
 
 void gpio_init() {
 
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOF, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOF, ENABLE);
 
 	GPIO_Init(_DBG_LED_PORT, &(GPIO_InitTypeDef){(1 << _DBG_LED_PIN), GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
-	GPIO_Init(_LED0_PORT, &(GPIO_InitTypeDef){(1 << _LED0_PIN), GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
 	GPIO_Init(_LED1_PORT, &(GPIO_InitTypeDef){(1 << _LED1_PIN), GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
 	GPIO_Init(_LED2_PORT, &(GPIO_InitTypeDef){(1 << _LED2_PIN), GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
+	GPIO_Init(_LED3_PORT, &(GPIO_InitTypeDef){(1 << _LED3_PIN), GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
 
-    GPIO_ResetBits(_LED0_PORT, (1 << _LED0_PIN));
-    GPIO_ResetBits(_LED1_PORT, (1 << _LED1_PIN));
-    GPIO_ResetBits(_LED2_PORT, (1 << _LED2_PIN));
+	GPIO_ResetBits(_LED1_PORT, (1 << _LED1_PIN));
+	GPIO_ResetBits(_LED2_PORT, (1 << _LED2_PIN));
+	GPIO_ResetBits(_LED3_PORT, (1 << _LED3_PIN));
 }
 
 int main(void) {
 	uint32_t nextBlink;
 	uint32_t blinkState = 0;
 
-    if (SysTick_Config(SystemCoreClock / 1000)) {
-        // Capture error
-        while (1){};
-    }
+	if (SysTick_Config(SystemCoreClock / 1000)) {
+		// Capture error
+		while (1){};
+	}
 
 	gpio_init();
 
-    uart_init();
+	uart_init();
 
-    i2cSetup(100000);
+	i2cSetup(100000);
 
-    usb2514b_init();
+	usb2514b_init();
 
-    USBD_Init(&USB_Device_dev,
-            &USR_desc,
-            &USBD_CDC_cb,
-            &USR_cb);
+	USBD_Init(&USB_Device_dev,
+			&USR_desc,
+			&USBD_CDC_cb,
+			&USR_cb);
 
 	nextBlink = tickMs + BLINK_DELAY_MS;
 	for(;;) {
